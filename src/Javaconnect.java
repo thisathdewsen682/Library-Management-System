fb0b0ad368a9bd1f7a35d355405ba15c0d4aed85
Thisath Dewsen <thisathdewsen77@gmail.com>
2023-03-29 17:01:48 +0530

    Intial Commit

---
+++ src/Javaconnect.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Thisath Dewsen
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+
+
+
+public class Javaconnect {
+    Connection conn;
+    
+    public static Connection ConnecrDB(){
+        
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/lms?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","");
+            return conn;
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null,"Database Connection failed");
+            e.printStackTrace();
+            return null;
+        }
+    }
+ 
+}
